6859a117a1766a1f38124a69d290da6f78ccecfa
Maaz <sheikh.59202@iqra.edu.pk>
2025-06-08 03:09:07 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // Allow external connections
+    port: 5173
+  }
 })
